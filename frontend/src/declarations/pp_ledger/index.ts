89ed4f9988801fd996a6b82d9b74050831ad8953
Charles <ponzi@musicalchairs.fun>
2026-04-20 10:06:01 -0700

    chore(frontend): generate pp_ledger candid bindings

---
+++ frontend/src/declarations/pp_ledger/index.ts
@@ -0,0 +1,3 @@
+export { idlFactory, init } from './pp_ledger.did.js';
+
+export type { _SERVICE, Account, TransferArg, TransferError, ApproveArgs, ApproveError, TransferFromArgs, TransferFromError } from './pp_ledger.did';
